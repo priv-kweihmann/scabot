1cf58ec82bf566164badb6b54d80eeb31e9233b8
Konrad Weihmann <kweihmann@outlook.com>
2021-08-11 19:56:19 +0200

    tests: add test for latest changes
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_class_changeset.py
+++ tests/test_class_changeset.py
@@ -38,3 +38,60 @@ class TestClassChangeset(TestBaseClass):
         assert(self.has_attribute(_diff, "Diff"))
         assert(self.has_attribute(_diff, "AffectedLines"))
         assert(self.has_attribute(_diff, "NewLines"))
+
+    @pytest.mark.parametrize('kwargs', [{
+        "provider": "mock",
+        "project": "1", 
+        "requestnum": "1", 
+        "inputfiles": [TestBaseClass.file_in_testdir('bad-po-1.0.dm')]
+    }])
+    def test_changeset_collection(self, kwargs):
+        import json
+        from scabot.changeset import Changeset
+
+        with open(os.path.join(self.TESTFILES_DIR, "{}_{}.json".format(kwargs["project"], kwargs["requestnum"]))) as i:
+            _json = json.load(i)
+            _diff = _json["projects"][kwargs["project"]]["requests"][kwargs["requestnum"]]["diffs"][0]
+
+            _changeset = Changeset(None)
+            _changeset.AddChangeFromCollection(_diff["diff"])
+
+            assert(any(x for x in _changeset.Changes if x.File == "classes/sca-global.bbclass"))
+
+    @pytest.mark.parametrize('kwargs', [{
+        "provider": "mock",
+        "project": "1", 
+        "requestnum": "1", 
+        "inputfiles": [TestBaseClass.file_in_testdir('bad-po-1.0.dm')]
+    }])
+    def test_changeset_collection_bytes(self, kwargs):
+        import json
+        from scabot.changeset import Changeset
+
+        with open(os.path.join(self.TESTFILES_DIR, "{}_{}.json".format(kwargs["project"], kwargs["requestnum"]))) as i:
+            _json = json.load(i)
+            _diff = _json["projects"][kwargs["project"]]["requests"][kwargs["requestnum"]]["diffs"][0]
+
+            _changeset = Changeset(None)
+            _changeset.AddChangeFromCollection(_diff["diff"].encode())
+
+            assert(any(x for x in _changeset.Changes if x.File == "classes/sca-global.bbclass"))
+
+    @pytest.mark.parametrize('kwargs', [{
+        "provider": "mock",
+        "project": "1", 
+        "requestnum": "7", 
+        "inputfiles": [TestBaseClass.file_in_testdir('bad-po-1.0.dm')]
+    }])
+    def test_changeset_collection_newfile(self, kwargs):
+        import json
+        from scabot.changeset import Changeset
+
+        with open(os.path.join(self.TESTFILES_DIR, "{}_{}.json".format(kwargs["project"], kwargs["requestnum"]))) as i:
+            _json = json.load(i)
+            _diff = _json["projects"][kwargs["project"]]["requests"][kwargs["requestnum"]]["diffs"][0]
+
+            _changeset = Changeset(None)
+            _changeset.AddChangeFromCollection(_diff["diff"])
+
+            assert(any(x for x in _changeset.Changes if x.File == "test-file"))
