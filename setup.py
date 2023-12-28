c7874f92bf545b7ce249485468b0c0ccd5a7d2db
priv-kweihmann <priv-kweihmann@localhost>
2023-12-28 11:21:05 +0000

    Release 1.5.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='scabot',
-    version='1.5.0',
+    version='1.5.1',
     author='Konrad Weihmann',
     author_email='kweihmann@outlook.com',
     description='SCA automation bot',
