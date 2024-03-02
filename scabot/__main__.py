c18ec477bf880a5077909c179f015ba77bfac55c
Konrad Weihmann <kweihmann@outlook.com>
2024-03-02 06:22:43 +0000

    Set CRYPTOGRAPHY_OPENSSL_NO_LEGACY
    before importing github
    
    Closes #137
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- scabot/__main__.py
+++ scabot/__main__.py
@@ -1,11 +1,16 @@
 # SPDX-FileCopyrightText: 2021 Konrad Weihmann
 # SPDX-License-Identifier: GPL-3.0-only
 
+# set CRYPTOGRAPHY_OPENSSL_NO_LEGACY to avoid errors
+# with newer cryptography versions
+import os
 import logging
 
-from scabot.runargs import create_parser
-from scabot.runargs import create_provider_instance
-from scabot.runargs import create_request_instance
+os.environ['CRYPTOGRAPHY_OPENSSL_NO_LEGACY'] = '1'
+
+from scabot.runargs import create_parser  # noqa: E402
+from scabot.runargs import create_provider_instance  # noqa: E402
+from scabot.runargs import create_request_instance  # noqa: E402
 
 
 def main():
@@ -13,7 +18,7 @@ def main():
     _prodiver = create_provider_instance(_args)
     logging.info(_prodiver)  # pragma: no cover
     _request = create_request_instance(_args, _prodiver)  # pragma: no cover
-    logging.info(f'New Notes -> {_request.NewNotes}') # noqa: G004 # pragma: no cover
+    logging.info(f'New Notes -> {_request.NewNotes}')  # noqa: G004 # pragma: no cover
     _request.Process()  # pragma: no cover
 
 
